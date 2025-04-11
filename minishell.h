e8fb56f551e293bec3cc553bc4fee2c3707ad82d
Khaled Hihi <khhihi@c4r9p5.1337.ma>
2025-04-11 16:54:02 +0100

    first commit

---
+++ minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khhihi <khhihi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 16:10:41 by khhihi            #+#    #+#             */
+/*   Updated: 2025/04/03 16:22:33 by khhihi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINI_SHELL_H
+#define MINI_SHELL_H
+
+# include <fcntl.h>
+# include <limits.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdbool.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
